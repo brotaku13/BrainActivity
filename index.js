0cd3664194518c5cfce54e7040fe06717bc0c4ea
brotaku13 <Brian.caulfield13@gmail.com>
2018-11-16 21:00:23 -0800

    cleaned up files a little bit

--- index.js
+++ index.js
@@ -32,17 +32,3 @@ document.addEventListener("DOMContentLoaded", function(){
         }
     })
 })
-
-// console.log('here')
-// const N = 300;
-// const gData = {
-//     nodes: [...Array(N).keys()].map(i => ({ id: i , fx: i*i, fy: i*i, fz: i*i})),
-//     links: [...Array(N).keys()]
-//     .filter(id => id)
-//     .map(id => ({
-//         source: id,
-//         target: Math.round(Math.random() * (id-1))
-//     }))
-// };
-// Graph(document.getElementById('3d-graph'))
-// .graphData(gData);
--- src/js/LoadData/LoadData.js
+++ src/js/LoadData/LoadData.js
@@ -1,34 +1,3 @@
-const CON_FILES = {
-    edge_list: '../../Data/CON/con_17_el',
-    weight_matrix: '../../Data/CON/con_17_mean_mat.txt',
-    coordinaates: '../../Data/CON/CON_region_centers_mm_coords_group_mean_3x200.txt',
-    node_ids: '../../Data/CON/CON_region_names_abbrev_group_mean_1x200.txt',
-    node_names: '../../Data/CON/CON_region_names_group_mean_1x200'
-}
-
-const OCD_FILES = {
-    edge_list: '../../Data/OCD/ocd_19_el',
-    weight_matrix: '../../Data/OCD/ocd_19_mean_mat.txt',
-    coordinaates: '../../Data/OCD/OCD_region_centers_mm_coords_group_mean_3x200.txt',
-    node_ids: '../../Data/OCD/OCD_region_names_abbrev_group_mean_1x200.txt',
-    node_names: '../../Data/OCD/OCD_region_names_group_mean_1x200'
-}
-
-function loadDataFromFile(groupName){
-    let file_group;
-    if(groupName === 'OCD'){
-        file_group = OCD_FILES;
-    } else {
-        file_group = CON_FILES
-    }
-    console.log('begin input');
-    parseCSV(file_group.edge_list);
-    parseCSV(file_group.coordinaates);
-    parseCSV(file_group.names);
-    parseCSV(file_group.node_names);
-    console.log('end input')
-}
-
 function parseCSV(inputFile, data_name) {
     Papa.parse(inputFile, {
         delimiter: ' ',
